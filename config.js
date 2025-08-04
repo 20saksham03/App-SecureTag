2e321ac3f1c84eb58fc61a4adff0dba73ba4cd40
Saksham Bansal <sakshambansal2003@gmail.com>
2025-08-05 00:41:33 +0530

    Update config.js

--- config.js
+++ config.js
@@ -4,7 +4,7 @@ const CONFIG = {
     // and Railway URL for production
     API_BASE_URL: window.location.hostname === 'localhost' 
         ? 'http://localhost:3000/api'
-        : 'https://app-securetag-production.up.railway.app/api', // Your Railway URL
+        : 'securetag.up.railway.app', // Your Railway URL
     
     APP_NAME: 'SecureTag',
     VERSION: '1.0.0',
