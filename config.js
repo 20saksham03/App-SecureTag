a00ccd8586921b71f105bbb6739d4d23e675a628
Saksham Bansal <sakshambansal2003@gmail.com>
2025-08-05 17:04:53 +0530

    Update config.js

--- config.js
+++ config.js
@@ -4,7 +4,7 @@ const CONFIG = {
     // and Railway URL for production
     API_BASE_URL: window.location.hostname === 'localhost' 
         ? 'http://localhost:3000/api'
-        : 'https://securetag.onrender.com/api', // Your Railway URL
+        : 'https://securetag.onrender.com/api', // Your render URL
     
     APP_NAME: 'SecureTag',
     VERSION: '1.0.0',
